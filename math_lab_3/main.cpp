aeee45f95595132971364f99f0e45c3516e0b457
sgakerru <sulmpx60@yandex.ru>
2019-04-21 22:44:17 +0400

    update math_lab3

--- math_lab_3/main.cpp
+++ math_lab_3/main.cpp
@@ -1,4 +1,4 @@
-//ver 1 
+//ver 1
 
 
 #include <iostream>
@@ -6,9 +6,15 @@
 #include <time.h>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 
 using namespace std;
+
+struct Points
+{
+    short int x, y, x_i, y_j;
+};
 
 typedef vector<bool> row;
 typedef vector<row> matrix;
@@ -25,7 +31,7 @@ void generation_vector (vector<int> &M, const int &a, const int &b)
 }
 
 //генерация вектора
-void generation_vector_ex2 (const vector<int> &A, vector<int> &X, int n, int m)
+void generation_vector_ex2 (const vector<int> &A, vector<int> &X, const int &n, const int &m)
 {
     int j = 0;
     for (int i = n-1; i > 0 && j < m; i--)
@@ -38,34 +44,13 @@ void generation_vector_ex2 (const vector<int> &A, vector<int> &X, int n, int m)
 //вывод массива
 void print_array (const vector<int> &M)
 {
-    for (vector<int>::const_iterator i = M.begin(); i != M.end(); ++i) cout << *i << " ";
+    for (vector<int>::const_iterator i = M.begin(); i != M.end(); ++i) cout << "\t" << *i;
     cout << endl;
-}
-
-void create_empty_matrix (matrix &A)
-{
-    row Row(5,0);
-    for (int i = 0; i < 5; i++)
-    {
-        A.push_back(Row);
-    }
-}
-void print_matrix (const matrix &A) // параметр const vector<int> &M
-{
-    for (int i = 0; i < 5; i++)
-    {
-       // cout << M[i] << '\t';
-        for(int j = 0; j < 5; j++)
-        {
-            cout << A[i][j] << '\t';
-        }
-        cout << endl;
-    }
 }
 
 void print_dec (const vector<int> &X, const vector<int> &Y)
 {
-    cout << "{ ";
+    cout << "Точки из последних элементов множеств : \n { ";
     for (vector<int>::const_iterator i = X.begin(); i != X.end(); ++i) {
         for (vector<int>::const_iterator j = Y.begin(); j != Y.end(); ++j) {
             cout << "(" << *i << "; " << *j << ")";
@@ -74,11 +59,97 @@ void print_dec (const vector<int> &X, const vector<int> &Y)
     cout << " }" << endl;
 }
 
+
+bool checknapovtor (const vector<Points> &P, const int &k, const int &k_x, const int &k_y)
+{
+    for (int i = 0; i < k; i++)
+    {
+        if(P[i].x_i == k_x)
+        {
+            return false;
+        }
+        if(P[i].y_j == k_y)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void find_points (vector<Points> &P, const vector<int> &A, const vector<int> &B, const int &n)
+{
+    int k = 0;
+    int k_x;
+    int k_y;
+    for (int i = 0; i < n; i++)
+    {
+        do {
+            k_x = rand() % n;
+            k_y = rand() % n;
+        } while (!checknapovtor(P, k, k_x, k_y));
+        P[i].x = A[k_x];
+        P[i].y = B[k_y];
+        P[i].x_i = k_x;
+        P[i].y_j = k_y;
+        k++;
+    }
+}
+void print_points(const vector<Points> &P)
+{
+    cout << "Случайный, неповноряющийся набор точек: \n { ";
+    for (vector<Points>::const_iterator i = P.begin(); i != P.end(); ++i) {
+            cout << "(" << i -> x << "; " << i -> y << ")";
+    }
+    cout << " }" << endl;
+}
+
+
+void create_empty_matrix (matrix &BOOL, const int &n)
+{
+    row Row(n,0);
+    for (int i = 0; i < n; i++)
+    {
+        BOOL.push_back(Row);
+    }
+}
+bool uslovie_ex (const int &i, const int &j, const vector<Points> &P) // условие отношения
+{
+    for (vector<Points>::const_iterator k = P.begin(); k != P.end(); ++k) {
+            if ( (k -> x_i == i) && (k -> y_j == j) ) return 1;
+    }
+    return 0;
+}
+void ex (matrix &BOOL, const vector<Points> &P, const int &n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            BOOL[i][j] = uslovie_ex(i, j, P);
+        }
+    }
+}
+void print_matrix (const matrix &BOOL, const vector<int> &A, const int &n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << "\t" << "|";
+        for(int j = 0; j < n; j++)
+        {
+            cout << "\t";
+            cout << setprecision (2) << fixed << BOOL[i][j];
+        }
+        cout << endl;
+        cout << setfill('-') << setw(113);
+        cout << "\n";
+        cout << setfill(' ');
+    }
+}
+
 int main()
 {
     srand((unsigned)time(0));
     system("chcp 1251 > nul");
-    cout << "Измайлов Наиль и Катунин Сергей, ДИПРБ-11. \n Дискретная математика, Лабораторная работа №2." << endl;
+    cout << "Измайлов Наиль и Катунин Сергей, ДИПРБ-11. \n Дискретная математика, Лабораторная работа №3." << endl;
     int n, a, b, m;
     cout << "Введите число n (размер множеств): ";
     cin >> n;
@@ -128,11 +199,22 @@ int main()
         print_array(X);
         print_array(Y);
         print_dec(X, Y);
-       /*
-        matrix A; // объявление матрицы
-        create_empty_matrix(A); // создание пустой матрицы
-        ex1(A, M); // заполнение матрицы в соответствие с заданием 1
-        print_matrix(A); // Вывод матрицы*/
+        vector<Points> P(n);
+        find_points(P, A, B, n);
+        print_points(P);
+        matrix BOOL; // объявление матрицы
+        create_empty_matrix(BOOL, n); // создание пустой матрицы
+        ex(BOOL, P, n); // заполнение матрицы в соответствие с заданием 1
+
+        cout << setw(3) << "\\" << setw(4) << "B" << setw(2) << "|" << endl;
+        cout << setw(4) << "\\" << setw(5) << "|" << endl;
+        cout << setw(2) << "A" << setw (3) << "\\" << setw(4) << "|";
+        print_array(B);
+        cout << setfill('-') << setw(113);
+        cout << "\n";
+        cout << setfill(' ');
+        print_matrix(BOOL, A, n); // Вывод матрицы
+
         cin >> ch;
         while (ch < 1 || ch > 2)
         {
