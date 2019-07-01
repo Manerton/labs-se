fd73063e4028c47f79c7a72911c07d6e90df9fff
sgakerru <sulmpx60@yandex.ru>
2019-07-01 23:19:56 +0400

    lab_16 fix

--- lab_16/main.cpp
+++ lab_16/main.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-using Pfun = double (*) (const double);  // объявление типа функции
+using Pfun = double (*) (const double);  // указатель на функцию
 
-const double eps = 1e-4;
+using Lfun = double (&) (const double);  // ссылка на функцию
+
+const double eps = 1e-4; // точность
 
 double fun_1( double x )
 {
@@ -29,7 +31,7 @@ double fun_4( double x )
     return sqrt(fabs(x));
 }
 
-double find_volume (Pfun fun, double Xn, double Xk, double h)
+double find_volume (Pfun fun, double Xn, double Xk, double h) // с помощью указателя
 {
     double result = 0;
     double half_h = h/2;
@@ -40,11 +42,10 @@ double find_volume (Pfun fun, double Xn, double Xk, double h)
         result += fun_result * fun_result; // по формуле объема используется квадрат исходной функции
     }
     result *= h;
-    //cout << result << endl;
     return result;
 }
 
-double integral (Pfun fun, double Xn, double Xk)
+double integral (Pfun fun, double Xn, double Xk) // с помощью указателя
 {
     double result, prev_result, h, delta;
     int n = 5;
@@ -62,12 +63,44 @@ double integral (Pfun fun, double Xn, double Xk)
     return result;
 }
 
+double find_volume_2 (Lfun fun, double Xn, double Xk, double h) // с помощью ссылки
+{
+    double result = 0;
+    double half_h = h/2;
+
+    for ( double x = Xn; x <= Xk; x += h )
+    {
+        double fun_result = fun(x + half_h) ;
+        result += fun_result * fun_result; // по формуле объема используется квадрат исходной функции
+    }
+    result *= h;
+    return result;
+}
+
+double integral_2 (Lfun fun, double Xn, double Xk) // с помощью ссылки
+{
+    double result, prev_result, h, delta;
+    int n = 5;
+    h = (Xk-Xn) / n; // делим фигуру на 5 прямоугольников
+    result = find_volume_2(fun, Xn, Xk, h); // находим площадь этих фигур методом средних прямоугольников
+
+    do {
+        n *= 2;
+        prev_result = result;
+        h = fabs(Xk-Xn) / n; // делим фигуру уже на 10 прямоугольников
+        result = find_volume_2(fun, Xn, Xk, h); // находим площадь этих фигур методом средних прямоугольников
+        delta = fabs(result - prev_result) / 3; // правило Рунге
+    } while (delta > eps);
+
+    return result;
+}
 
 int main()
 {
     system("chcp 1251 > nul");
     cout << "Лабораторная работа №16. Вариант 13.\nАвтор: Катунин Сергей. ДИПРБ-11.\n" << endl;
-    Pfun Arr_fun[4] {fun_1, fun_2, fun_3, fun_4};
+    Pfun Arr_fun[] {fun_1, fun_2, fun_3, fun_4}; // массив указателей на функции, или, грубо говоря, массив функций
+                                                 // так как лень делать свитч кейсы
     int nomer;
     cout << "Найти объём тела вращения методом прямоугольников с заданной точностью 1e-4." << endl;
     cout << "1. Функция x" << endl;
@@ -92,14 +125,17 @@ int main()
         Arr_fun[nomer-1](Xk);
         cout << "Функция существует на границах интегрирования." << endl;
         cout << "Переходим к поиску объема тела." << endl;
-        double result;
+        double result_pointer, result_link;
         if (Xn < Xk)
         {
-            result = integral(Arr_fun[nomer-1], Xn, Xk);
+            result_pointer = integral(Arr_fun[nomer-1], Xn, Xk); // ищу объем с помощью функции зависящей от указателя
+            result_link = integral_2(*Arr_fun[nomer-1], Xn, Xk); // ищу объем с помощью функции зависящей от ссылки, для этого разыменовываю указатель на функцию и передаю саму функцию
         } else {
-            result = -integral(Arr_fun[nomer-1], Xk, Xn);
+            result_pointer = -integral(Arr_fun[nomer-1], Xk, Xn);
+            result_link = -integral_2(*Arr_fun[nomer-1], Xk, Xn);
         }
-        cout << "Объем равен " << fixed <<setprecision(4) << result << " * Pi" << endl;
+        cout << "(С помощью указателя) Объем равен " << fixed <<setprecision(4) << result_pointer << " * Pi" << endl;
+        cout << "(С помощью ссылки) Объем равен " << fixed <<setprecision(4) << result_link << " * Pi" << endl;
     }
     catch (const char* exception) // обработка исключений типа const char*
     {
--- tests/experiment/main.cpp
+++ tests/experiment/main.cpp
@@ -4,36 +4,14 @@
 #include <algorithm>
 
 using namespace std;
-typedef set<int, less<int> > set_type;
 
 int main()
 {
-    set_type M1 {1, 4, 6};
-    set_type M2 {2, 4, 6};
-    set_type M3 {1, 3, 2};
-    set_type M4 {7, 3, 6};
-
-    set_type temp;
-    set_type M12;
-    set_intersection (M1.begin(), M1.end(), M2.begin(), M2.end(), inserter(M12, M12.begin()));
-    set_intersection (M1.begin(), M1.end(), M2.begin(), M2.end(), inserter(temp, temp.begin()));
-    cout << M12.size();
-    set_type M13;
-    set_intersection (M1.begin(), M1.end(), M3.begin(), M3.end(), inserter(M13, M13.begin()));
-    set_intersection (M1.begin(), M1.end(), M3.begin(), M3.end(), inserter(temp, temp.begin()));
-    cout << M13.size();
-    set_type result;
-    set_union (M12.begin(), M12.end(), M13.begin(), M13.end(), inserter(result, result.begin()));
-    cout << result.size();
-    cout << temp.size();
-    /*set_type M14;
-    set_intersection (M1.begin(), M1.end(), M4.begin(), M4.end(), inserter(M14, M14.begin()));
-    set_type M23;
-    set_intersection (M2.begin(), M2.end(), M3.begin(), M3.end(), inserter(M23, M23.begin()));
-    set_type M24;
-    set_intersection (M2.begin(), M2.end(), M4.begin(), M4.end(), inserter(M24, M24.begin()));
-    set_type M34;
-    set_intersection (M3.begin(), M3.end(), M4.begin(), M4.end(), inserter(M34, M34.begin()));
-*/
+    int a = 5;
+    cout << a;
+    int *b = &a;
+    cout << *b;
+    int c = 3 + *b;
+    cout << c;
     return 0;
 }
