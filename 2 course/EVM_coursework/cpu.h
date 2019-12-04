0903697ea85eba174e7ca28ed552677085d31f57
SgAkErRu <sulmpx60@yandex.ru>
2019-12-05 00:40:38 +0400

    EVM coursework in progress

--- 2 course/EVM_coursework/cpu.h
+++ 2 course/EVM_coursework/cpu.h
@@ -5,6 +5,51 @@
 #include "types.h"
 #include "memory.h"
 
+enum class COP
+{
+    stop = 0, // -- команда остановки -- //
+    empty = 1, // -- пустая команда-заглушка -- //
+// -- команды пересылки данных -- //
+    exchange = 2, // -- обменять содержимое регистров -- //
+    move = 3, // -- пересылка <регистр источник> <регистр назначения> -- //
+    load = 4, // -- пересылка память - регистр (в r2) -- //
+    save = 5, // -- пересылка регистр (r1) - память -- //
+    loadaddress = 6, // -- загрузка константы-адреса в регистр -- //
+    // -- продумать еще команды для разных видов адресаций -- //
+// -- команды целой арифметики -- //
+    inc = 7,
+    add = 8,
+    dec = 9,
+    sub = 10,
+    mul = 11,
+    div = 12,
+// -- команды дробной арифметики -- //
+    finc = 13,
+    fadd = 14,
+    fdec = 15,
+    fsub = 16,
+    fmul = 17,
+    fdiv = 18,
+// -- команды ввода и вывода -- //
+    in_int = 19,
+    out_int = 20,
+    in_uint = 21,
+    out_uint = 22,
+    in_float = 23,
+    out_float = 24,
+// -- команды перехода -- //
+    jmp = 25,
+    jmp_offset = 26,
+    jmp_ptr = 27,
+    je = 28, // -- jump if equal, ZF = 1 -- //
+    jne = 29, // -- jump if not equal, ZF = 0 -- //
+
+    call = ,
+    ret = ,
+
+// -- команды управления состоянием ЦП -- //
+};
+
 class CPU
 {
     struct PSW
@@ -18,17 +63,28 @@ class CPU
             uint16_t PINF : 1;      // флаг - результат +бесконечность
             uint16_t NINF : 1;      // флаг - результат -бесконечность
             uint16_t reserved : 10;  // в резерве
-        } FLAGS;
+        };
 
         uint16_t IP = 0;
+        FLAGS FLAGS;
 
     };
 
     cmd cmd;
 
 public:
+    PSW PSW;      // -- PSW = IP + Flags -- //
+    Memory ram;   // -- память, слова - 32 бита, размер адреса 16 бит -- //
+    data_t r[16]; // -- регистры общего назначения -- //
+
     CPU();
-    PSW PSW;
+    ~CPU();
+
+    CPU(const CPU &) = delete;
+    CPU(const CPU &&) = delete;
+    CPU& operator=(const CPU &)  = delete;
+    CPU& operator=(const CPU &&) = delete;
+
 };
 
 #endif // CPU_H
--- 2 course/EVM_coursework/main.cpp
+++ 2 course/EVM_coursework/main.cpp
@@ -12,26 +12,116 @@ bool Uploader(CPU &cpu, const string &filename)
 
     if (!file.is_open()) return false;
 
-    uint16_t startAddress = 0;  
-    char symbol = ' ';
-    while (symbol != 'e')
+    uint16_t address = 0;   // -- адрес памяти, по которому загружаются данные / команды -- //
+    char symbol = ' ';  // -- текущий читаемый символ -- //
+    while (symbol != 'e')   // -- пока не встретили символ 'e' -- //
     {
         string s;
         getline(file, s);
-        if (!s.empty())
+        istringstream ss(s);
+        ss >> symbol;
+        switch (symbol)
         {
-
+            case 'a':   // -- установить адрес -- //
+            {
+                ss >> address;
+                break;
+            }
+            case 'i':   // -- загрузка целого числа -- //
+            {
+                data_t integer;
+                ss >> integer.i;
+                cpu.ram.push(integer, address);
+                ++address;
+                break;
+            }
+            case 'u':   // -- загрузка беззнакового числа -- //
+            {
+                data_t uinteger;
+                ss >> uinteger.u;
+                cpu.ram.push(uinteger, address);
+                ++address;
+                break;
+            }
+            case 'f':   // -- загрузка дробного числа -- //
+            {
+                data_t real;
+                ss >> real.f;
+                cpu.ram.push(real, address);
+                ++address;
+                break;
+            }
+            case 'c':   // -- загрузка команды -- //
+            {
+                data_t command; // -- пример: c t cop r1 r2 (для 16-битной команды) -- //
+                uint16_t temp;
+                ss >> temp;     // -- читаем флаг длины команды t -- //
+                if (temp == 0)  // -- если флаг t (type) равен 0, значит команда короткая (16 бит) -- //
+                {
+                    command.c.c16[0].t = 0;
+                    ss >> temp; // -- читаем cop (код операции) -- //
+                    command.c.c16[0].cop = uint8_t(temp);
+                    ss >> temp; // -- читаем r1 -- //
+                    command.c.c16[0].r1 = uint8_t(temp);
+                    ss >> temp; // -- читаем r2 -- //
+                    command.c.c16[0].r2 = uint8_t(temp);
+                    getline(file,s);    // -- если была указана короткая команда, //
+                    ss.str(s);          // значит следующая строчка в файле тоже должна быть короткой командой -- //
+                    ss >> symbol;   // -- читаем символ -- //
+                    ss >> temp;     // -- читаем тип команды -- //
+                    command.c.c16[1].t = 0;
+                    ss >> temp; // -- читаем cop (код операции) -- //
+                    command.c.c16[1].cop = uint8_t(temp);
+                    ss >> temp; // -- читаем r1 -- //
+                    command.c.c16[1].r1 = uint8_t(temp);
+                    ss >> temp; // -- читаем r2 -- //
+                    command.c.c16[1].r2 = uint8_t(temp);
+                }
+                else // -- иначе длинная -- //
+                {
+                    command.c.c32.t = 1;
+                    ss >> temp; // -- читаем cop (код операции) -- //
+                    command.c.c32.cop = uint8_t(temp);
+                    ss >> temp; // -- читаем r1 -- //
+                    command.c.c32.r1 = uint8_t(temp);
+                    ss >> temp; // -- читаем r2 -- //
+                    command.c.c32.r2 = uint8_t(temp);
+                    ss >> temp; // -- читаем address -- //
+                    command.c.c32.address = uint8_t(temp);
+                }
+                cpu.ram.push(command, address);
+                ++address;
+                break;
+            }
+            case 'e':
+            {
+                data_t command;
+                command.c.c32.t = 1;
+                command.c.c32.cop = 0;
+                cpu.ram.push(command,address);
+                uint16_t IP;
+                ss >> IP;
+                cpu.PSW.IP = IP;
+                break;
+            }
         }
     }
+    file.close();
+    return true;
 }
 
 int main (int argc, char* argv[])
 {
-    //CPU cpu;
+    CPU cpu;
     if (argc > 1)
     {
         cout << "File " << argv[1] << " is ";
-
+        if (Uploader(cpu, argv[1]))
+        {
+            cout << " uploaded!" << endl;
+            //cpu.run();
+        }
+        else cout << " not uploaded" << endl;
     } else cout << "Programm has been started without arguments." << endl;
     system("pause");
 
