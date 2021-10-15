e9444a6de8096c0ea9bf650826e5337eb7bd7052
SgAkErRu <sulmpx60@yandex.ru>
2021-10-16 00:45:31 +0400

    Mobile Programming -> Простенькая прога с кнопкой и editText.

--- 4 course/mobile_programming/AndroidApp/app/src/main/java/ru/sgakerru/androidapp/MainActivity.kt
+++ 4 course/mobile_programming/AndroidApp/app/src/main/java/ru/sgakerru/androidapp/MainActivity.kt
@@ -3,14 +3,29 @@ package ru.sgakerru.androidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import ru.sgakerru.androidapp.databinding.ActivityMainBinding;
+import android.view.View;
+import android.widget.Toast;
+import java.util.Calendar;
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding;
 
+    private val buttonAgeOnClickListener = (View.OnClickListener {
+        val year : Int? = this.binding.editTextDateAge.text.toString().toIntOrNull();
+
+        if (year != null)
+        {
+            val res: Int = Calendar.getInstance().get(Calendar.YEAR) - year;
+            this.binding.txtViewAge.text = res.toString();
+            Toast.makeText(applicationContext, "Возраст подсчитан: $res!", Toast.LENGTH_SHORT).show();
+        }
+    });
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         this.binding = ActivityMainBinding.inflate(layoutInflater);
+        this.binding.buttonAge.setOnClickListener(buttonAgeOnClickListener);
         this.setContentView(binding.root);
     }
 }
